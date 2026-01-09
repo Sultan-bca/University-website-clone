184a1093b16829084c4dcbbff08604b92d423bd6
Sultan-bca <sultan965499sk@gmail.com>
2026-01-09 19:54:37 +0530

    Add files via upload

---
+++ Components/Navbar/Navbar.jsx
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-scroll';   // ✅ Correct import
+import educity from "../../assets/educity.png";
+import menu_icon from "../../assets/menu_icon.png";
+import './Navbar.css';
+
+const Navbar = () => {
+
+    const [sticky, setSticky] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 50) {
+                setSticky(true);
+            } else {
+                setSticky(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
+    const [mobileMenu, setMobileMenu] = useState(false)
+    const toggleMenu=()=>{
+        mobileMenu?setMobileMenu(false):setMobileMenu(true);
+    }
+
+    return (
+        <nav className={`container ${sticky ? "dark-nav" : ""}`}>
+            <img src={educity} alt="" className='logo'/>
+
+            <ul className={mobileMenu?'':'hide-mobile-menu'}>
+                <li>
+                    <Link to='hero' smooth={true} offset={0} duration={500}>
+                        Home
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to='programs' smooth={true} offset={-280} duration={500}>
+                        Program
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to='about' smooth={true} offset={-170} duration={500}>
+                        About us
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to='campus' smooth={true} offset={-260} duration={500}>
+                        Campus
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to='testimonials' smooth={true} offset={-325} duration={500}>
+                        Testimonials
+                    </Link>
+                </li>
+
+                <li>
+                    <Link to='contact' smooth={true} offset={-260} duration={500} className='btn'>
+                        Contact us
+                    </Link>
+                </li>
+            </ul>
+            <img src={menu_icon} alt='' className='menu-icon' onClick={toggleMenu}/>
+        </nav>
+    );
+}
+
+export default Navbar;
